595923d42154ebf50a44328a2c90f0d1e84174a5
Ubuntu <ubuntu@ip-172-31-10-200.us-west-2.compute.internal>
2026-04-29 00:32:39 +0000

    Configure AMCL localization project files

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("my_robot")
-subdirs("ball_chaser")
+subdirs("pgm_map_creator")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,8 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/ubuntu/catkin_ws/build/gtest/cmake_install.cmake")
-  include("/home/ubuntu/catkin_ws/build/my_robot/cmake_install.cmake")
-  include("/home/ubuntu/catkin_ws/build/ball_chaser/cmake_install.cmake")
+  include("/home/ubuntu/catkin_ws/build/pgm_map_creator/cmake_install.cmake")
 
 endif()
 
